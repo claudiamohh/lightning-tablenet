cfb54ffe7e1f40e7bae958eb87d03e02ba6ad4d0
Claudia <claudiamhch@gmail.com>
2022-08-17 16:10:32 +0800

    Refactor train script and update model from Lightning_TableNet class

---
+++ train.py
@@ -0,0 +1,55 @@
+import albumentations as A
+import torch
+import pytorch_lightning as pl
+from albumentations.pytorch.transforms import ToTensorV2
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import (
+    ModelCheckpoint,
+    EarlyStopping,
+    LearningRateMonitor,
+)
+from dataset import Lightning_MarmotDataset
+from model import Lightning_TableNet
+
+
+image_size = (896, 896)
+
+train_transform = A.Compose(
+    [
+        A.Resize(1024, 1024, always_apply=True),
+        A.RandomResizedCrop(*image_size, scale=(0.7, 1.0), ratio=(0.7, 1)),
+        A.HorizontalFlip(),
+        A.VerticalFlip(),
+        A.Normalize(),
+        ToTensorV2(),
+    ]
+
+
+test_transform = A.Compose(
+    [A.Resize(*image_size, always_apply=True), A.Normalize(), ToTensorV2()]
+)
+
+dataset = Lightning_MarmotDataset(data_dir="C:/Users/Claudia/Desktop/git/lightning_tablenet/data/Marmot_data/", train_transform=train_transform, test_transform=test_transform, batch_size=2)
+
+model = Lightning_TableNet(num_class=1, encoder='vgg')
+
+EXPERIMENT_NAME = f"{model.__class__.name__}"
+
+checkpoint_callback = ModelCheckpoint(monitor='validation_loss', save_top_k=1, save_last=True, mode='min')      
+
+early_stop_callback = EarlyStopping(monitor='validaton_loss', mode='min', patience=10)  
+
+lr_monitor = LearningRateMonitor(logging_interval='step')   
+
+trainer = pl.Trainer(accumulate_grad_batches=2,
+                    gradient_clip_val=0.5, 
+                    callbacks=[lr_monitor, checkpoint_callback, early_stop_callback], 
+                    logger=TensorBoardLogger('lightning_tablenet', name="tablenet_baseline_adam"), 
+                    max_epochs=5000,
+                    gpus=1 if torch.cuda.is_available() else None)  
+
+trainer.fit(model, 
+            datamodule=dataset)
+
+trainer.test(model,
+             datamodule=dataset)
