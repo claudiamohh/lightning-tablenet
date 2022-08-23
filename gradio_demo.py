027a4254003ddc1ef8583ca639f14fb9faf58f63
Claudia <claudiamhch@gmail.com>
2022-08-23 18:22:37 +0800

    Fix formatting

--- gradio_demo.py
+++ gradio_demo.py
@@ -1,6 +1,5 @@
 from collections import OrderedDict
 from typing import List
-
 import numpy as np
 import pandas as pd
 from albumentations import Compose
@@ -8,27 +7,31 @@ from PIL import Image
 from predict import PredictImage
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
-import gradio as gr 
-
-transforms = A.Compose([
-        A.Resize(896, 896, always_apply=True),
-        A.Normalize(),
-        ToTensorV2()
-    ])
+import gradio as gr
 
 
-model_path = 'pretrained_models/tablenet_baseline_adam_gradclipping.ckpt'
+transforms = A.Compose(
+    [A.Resize(896, 896, always_apply=True), A.Normalize(), ToTensorV2()]
+)
 
-examples = ['examples/10.1.1.160.708_11.bmp', 'examples/10.1.1.193.1812_24.bmp', 'examples/10.1.1.1.2071_4.bmp']
+
+model_path = "pretrained_models/tablenet_baseline_adam_gradclipping.ckpt"
+
+examples = [
+    "examples/10.1.1.160.708_11.bmp",
+    "examples/10.1.1.193.1812_24.bmp",
+    "examples/10.1.1.1.2071_4.bmp",
+]
 
 predictor = PredictImage(model_path, transforms)
 
+
 def predict_function(image):
-    """ 
+    """
     Function to predict table content using PredictImage
 
-    Args: 
-        Image (pil): Image to predict content   
+    Args:
+        Image (pil): Image to predict content
 
     Returns List[pd.DataFrame]: Contents of table in dataframe format
     """
@@ -36,8 +39,10 @@ def predict_function(image):
     return output[0] if output else pd.DataFrame()
 
 
-gr.Interface(predict_function,
-             gr.components.Image(type='pil'),
-             gr.Dataframe(type="pandas", label="Dataframe"),
-             title = 'TableNet Demo',
-             examples = examples).launch(share=True)
+gr.Interface(
+    predict_function,
+    gr.components.Image(type="pil"),
+    gr.Dataframe(type="pandas", label="Dataframe"),
+    title="TableNet Demo",
+    examples=examples,
+).launch(share=True)
