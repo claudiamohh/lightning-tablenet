362cc042731404502e5329031c5f34266648945d
Claudia <claudiamhch@gmail.com>
2022-08-23 12:31:12 +0800

    Add gradio_demo.py

---
+++ gradio_demo.py
@@ -0,0 +1,43 @@
+from collections import OrderedDict
+from typing import List
+
+import numpy as np
+import pandas as pd
+from albumentations import Compose
+from PIL import Image
+from predict import PredictImage
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+import gradio as gr 
+
+transforms = A.Compose([
+        A.Resize(896, 896, always_apply=True),
+        A.Normalize(),
+        ToTensorV2()
+    ])
+
+
+model_path = 'pretrained_models/tablenet_baseline_adam_gradclipping.ckpt'
+
+predictor = PredictImage(model_path, transforms)
+
+def predict_function(image):
+    """ 
+    Function to predict table content using PredictImage
+
+    Args: 
+        Image (pil): Image to predict content   
+
+    Returns List[pd.DataFrame]: Contents of table in dataframe format
+    """
+    output = predictor.predict(image)
+    return output[0] if output else pd.DataFrame()
+
+examples = ['examples/10.1.1.160.708_11.bmp', 'examples/10.1.1.193.1812_24.bmp', 'examples/10.1.1.1.2071_4.bmp']
+
+
+gr.Interface(predict_function,
+             gr.components.Image(type='pil'),
+             gr.Dataframe(type="pandas", label="Dataframe"),
+             title = 'TableNet Demo',
+             examples = examples).launch(share=True)
