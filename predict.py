e8f82b6956683cf7b8a1557fdb47f2ba85e4ce6c
Claudia <claudiamhch@gmail.com>
2022-08-24 10:59:22 +0800

    Remove comments

--- predict.py
+++ predict.py
@@ -28,14 +28,14 @@ class PredictImage:
         self.threshold = threshold
         self.per = per
 
-        self.model = LightningTableNet.load_from_checkpoint(checkpoint_path)   #restore TableNet model from the checkpointed state (model with gradient clipping)
-        self.model.eval()     #Turn off specific layers of the model for evaluation 
-        self.model.requires_grad_(False)    #Freezing parameters of model 
+        self.model = LightningTableNet.load_from_checkpoint(checkpoint_path)   
+        self.model.eval()      
+        self.model.requires_grad_(False)     
     
     def predict(self, image: Image) -> List[pd.DataFrame]:
-        processed_image = self.transforms(image=np.array(image))["image"] #undergoes transformation 
+        processed_image = self.transforms(image=np.array(image))["image"] 
 
-        table_mask, column_mask = self.model.forward(processed_image.unsqueeze(0))  #Getting table, column masks from transformed images
+        table_mask, column_mask = self.model.forward(processed_image.unsqueeze(0))  
 
         table_mask = self.add_threshold(table_mask)  
         column_mask = self.add_threshold(column_mask) 
@@ -49,12 +49,12 @@ class PredictImage:
                 cols = []
                 for column in segmented_columns.values():
                     cols.append(self.column_to_dataframe(column, image))
-                tables.append(pd.concat(cols, ignore_index=True, axis=1)) #concatenate along columns
+                tables.append(pd.concat(cols, ignore_index=True, axis=1)) 
         return tables 
 
     def add_threshold(self, mask):
         mask = mask.squeeze(0).squeeze(0).numpy() > self.threshold
-        return mask.astype(int) #convert numpy.ndarray to int
+        return mask.astype(int) 
 
     def process_tables(self, segmented_tables):
         width, height = segmented_tables.shape
@@ -79,9 +79,9 @@ class PredictImage:
 
     @staticmethod
     def segment_image(image):
-        threshold = threshold_otsu(image) #finding the threshold value automatically using Otsu's method
-        bw = closing(image > threshold, square(2)) #removes dark spots and connect small bright cracks
-        removed = clear_border(bw) #clear objects connected to the label image border
+        threshold = threshold_otsu(image) 
+        bw = closing(image > threshold, square(2)) 
+        removed = clear_border(bw) 
         label_image = label(removed)
         return label_image
 
@@ -93,6 +93,6 @@ class PredictImage:
         crop = column * image
         white = np.ones(column.shape) * invert(column) * 255
         crop = crop + white
-        ocr = image_to_string(Image.fromarray(crop.astype(np.uint8))) #using ocr tesseract
+        ocr = image_to_string(Image.fromarray(crop.astype(np.uint8))) 
         return pd.DataFrame({"col": [value for value in ocr.split("\n") if len(value) > 0]})
 
