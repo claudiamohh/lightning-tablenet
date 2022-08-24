aaebd6a872b1f33c018a2d3d4579299059b9df0d
Claudia <claudiamhch@gmail.com>
2022-08-24 10:47:24 +0800

    Remove torch.nn.functional library

--- train.py
+++ train.py
@@ -8,7 +8,6 @@ from pytorch_lightning.callbacks import (
     EarlyStopping,
     LearningRateMonitor,
 )
-from torch.nn import functional as F
 from datasets.marmot import LightningMarmotDataset
 from models.tablenet import LightningTableNet
 
@@ -24,29 +23,38 @@ train_transform = A.Compose(
         A.Normalize(),
         ToTensorV2(),
     ]
-    )
+)
 
 test_transform = A.Compose(
     [A.Resize(*image_size, always_apply=True), A.Normalize(), ToTensorV2()]
 )
 
-dataset = LightningMarmotDataset(data_dir="./data/Marmot_data/", train_augmentation=train_transform, test_processing=test_transform, batch_size=1)
+dataset = LightningMarmotDataset(
+    data_dir="./data/Marmot_data/",
+    train_augmentation=train_transform,
+    test_processing=test_transform,
+    batch_size=1,
+)
 
-model = LightningTableNet(num_class=1, encoder='vgg')
+model = LightningTableNet(num_class=1, encoder="vgg")
 
-checkpoint_callback = ModelCheckpoint(monitor='val_loss', save_top_k=1, save_last=True, mode='min')      
-early_stop_callback = EarlyStopping(monitor='val_loss', mode='min', patience=10)  
-lr_monitor = LearningRateMonitor(logging_interval='step')   
+checkpoint_callback = ModelCheckpoint(
+    monitor="val_loss", save_top_k=1, save_last=True, mode="min"
+)
+early_stop_callback = EarlyStopping(monitor="val_loss", mode="min", patience=10)
+lr_monitor = LearningRateMonitor(logging_interval="step")
 
-trainer = pl.Trainer(accumulate_grad_batches=2,
-                    gradient_clip_val=0.5, 
-                    callbacks=[lr_monitor, checkpoint_callback, early_stop_callback], 
-                    logger=TensorBoardLogger('lightning_tablenet', name="tablenet_baseline_adam_gradclipping"), 
-                    max_epochs=5000,
-                    gpus=1 if torch.cuda.is_available() else None)  
+trainer = pl.Trainer(
+    accumulate_grad_batches=2,
+    gradient_clip_val=0.5,
+    callbacks=[lr_monitor, checkpoint_callback, early_stop_callback],
+    logger=TensorBoardLogger(
+        "lightning_tablenet", name="tablenet_baseline_adam_gradclipping"
+    ),
+    max_epochs=5000,
+    gpus=1 if torch.cuda.is_available() else None,
+)
 
-trainer.fit(model, 
-            datamodule=dataset)
+trainer.fit(model, datamodule=dataset)
 
-trainer.test(model,
-             datamodule=dataset)
+trainer.test(model, datamodule=dataset)
